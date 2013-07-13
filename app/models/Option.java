94cc96048e451997d080e4cd4e39ca8a29e8c128
Test <testovich>
2013-07-13 14:45:20 +0300

    added models

---
+++ app/models/Option.java
@@ -0,0 +1,21 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+
+@Entity
+public class Option extends Model {
+
+    @Id
+    public String id;
+
+    public String name;
+
+    public String description;
+
+    //public String link;
+
+    //public String imgURL;
+}
---
+++ app/models/Poll.java
@@ -0,0 +1,19 @@
+package models;
+
+import play.db.ebean.Model;
+
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import java.util.List;
+
+@Entity
+public class Poll extends Model {
+
+    @Id
+    public String id;
+
+    public String name;
+
+    public List<Option> options;
+
+}
